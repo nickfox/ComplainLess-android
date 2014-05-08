48b032ea20166ac6ca4abb42085c3d81055db180
Nick Fox <nicolaspatrickfox@hotmail.com>
2014-05-07 20:22:32 -0700

    added new images

--- app/src/main/java/com/websmithing/complainless/ComplainLessActivity.java
+++ app/src/main/java/com/websmithing/complainless/ComplainLessActivity.java
@@ -1,9 +1,12 @@
 package com.websmithing.complainless;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 
+import java.util.Date;1
 
 public class ComplainLessActivity extends ActionBarActivity {
     private static final String TAG = "ComplainLessActivity";
@@ -16,6 +19,17 @@ public class ComplainLessActivity extends ActionBarActivity {
 
         flipMeter = (Flipmeter) findViewById(R.id.Flipmeter);
 
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.websmithing.complainless.prefs", Context.MODE_PRIVATE);
+
+        if (sharedPreferences.getBoolean("isFirstLaunch", true)) {
+            Date now = new Date();
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putLong("startDate", now.getTime());
+            editor.putLong("currentBestDate", now.getTime());
+            editor.putBoolean("isFirstLaunch", false);
+            editor.putInt("personalBest", 0);
+            editor.commit();
+        }
     }
 
     @Override
